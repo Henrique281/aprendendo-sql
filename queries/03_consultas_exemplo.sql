5405cb188a4dbfe0dfabce2ff5158718561d3855
Henrique Freitas <133824049+Henrique281@users.noreply.github.com>
2025-08-16 02:29:45 -0300

    Create 03_consultas_exemplo.sql
    consultas

---
+++ queries/03_consultas_exemplo.sql
@@ -0,0 +1,8 @@
+-- Seleciona o banco
+USE Estudos;
+
+-- Consultar todos os dados da tabela aluno
+SELECT * FROM aluno;
+
+-- Consultar alunos com idade maior que 20
+SELECT * FROM aluno WHERE idade > 20;
